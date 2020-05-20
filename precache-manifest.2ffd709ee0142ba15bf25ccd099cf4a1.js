0a410f3a57b326d20ac55cd96cba9db64c892e83
Acllex <986169823@qq.com>
2020-05-20 09:16:03 +0800

    尝试部署

---
+++ precache-manifest.2ffd709ee0142ba15bf25ccd099cf4a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735f89d10273054ebb49d3d195d09848",
+    "url": "img/logo.735f89d1.jpg"
+  },
+  {
+    "revision": "af6edbe3b8c831716c5ac3f7368d0db2",
+    "url": "index.html"
+  },
+  {
+    "revision": "68bab2ccddd9d8957fab",
+    "url": "js/app.a76f22ca.js"
+  },
+  {
+    "revision": "0a4686eff24b13d43c87",
+    "url": "js/chunk-3275eef2.4ee7907a.js"
+  },
+  {
+    "revision": "736b8f4673d37d72d79d",
+    "url": "js/chunk-3bd674cb.8beea68c.js"
+  },
+  {
+    "revision": "539952b252c72cb2d13c",
+    "url": "js/chunk-465524e7.a34d4280.js"
+  },
+  {
+    "revision": "3c27dfba1a8fe2a0a053",
+    "url": "js/chunk-472ab4a0.c7b72ac9.js"
+  },
+  {
+    "revision": "641c1daa15bf1df0b8b8",
+    "url": "js/chunk-5834fb56.18b2230c.js"
+  },
+  {
+    "revision": "16eb2de57c7be82a72fb",
+    "url": "js/chunk-8c7a7444.f67be1e2.js"
+  },
+  {
+    "revision": "3fafdb7c735c435579a8",
+    "url": "js/chunk-vendors.965aff27.js"
+  },
+  {
+    "revision": "15bd0687efebae798955d1c90acda863",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
